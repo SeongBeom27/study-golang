82eb5733c466951f3daa4fe173a9136957d90a41
SeongBeom lee <52270698+SeongBeom27@users.noreply.github.com>
2022-05-29 17:18:02 +0900

    정적 파일 내용을 전달하는 미들웨어 작성

--- webbam/main.go
+++ webbam/main.go
@@ -16,17 +16,20 @@ func main() {
 		fmt.Fprintln(c.ResponseWriter, "about")
 	})
 
-	r.HandleFunc("GET", "/users/:id", func(c *Context) {
+	r.HandleFunc("GET", "/users/:id", logHandler(recoverHandler(func(c *Context) {
+		if c.Params["id"] == "0" {
+			panic("id is zero")
+		}
 		fmt.Fprintf(c.ResponseWriter, "retrieve user %v\n", c.Params["id"])
-	})
+	})))
 
 	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *Context) {
 		fmt.Fprintf(c.ResponseWriter, "retrieve user's address %v's address %v\n", c.Params["user_id"], c.Params["address_id"])
 	})
 
-	r.HandleFunc("POST", "/users", func(c *Context) {
+	r.HandleFunc("POST", "/users", logHandler(recoverHandler(parseFormHandler(parseJsonBodyHandler(func(c *Context) {
 		fmt.Fprintln(c.ResponseWriter, "create user")
-	})
+	})))))
 
 	r.HandleFunc("POST", "/users/:user_id/addresses", func(c *Context) {
 		fmt.Fprintf(c.ResponseWriter, "create user %v's address\n", c.Params["user_id"])
--- webbam/middleware.go
+++ webbam/middleware.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"path"
+	"strings"
 	"time"
 )
 
@@ -21,5 +26,110 @@ func logHandler(next HandlerFunc) HandlerFunc {
 			c.Request.Method,
 			c.Request.URL.String(),
 			time.Now().Sub(t))
+	}
+}
+
+// error 처리 미들웨어 작성
+func recoverHandler(next HandlerFunc) HandlerFunc {
+	return func(c *Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %+v", err)
+				http.Error(c.ResponseWriter,
+				http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+			}
+		}()
+		next(c)
+	}
+}
+
+func parseFormHandler(next HandlerFunc) HandlerFunc {
+	return func(c *Context) {
+		c.Request.ParseForm()
+		fmt.Println(c.Request.PostForm)
+		for k, v := range c.Request.PostForm {
+			// v가 존재할 경우
+			if len(v) > 0 {
+				c.Params[k] = v[0]
+			}
+		}
+		next(c)
+	}
+}
+
+func parseJsonBodyHandler(next HandlerFunc) HandlerFunc {
+	return func(c *Context) {
+		var m map[string]interface{}
+		if json.NewDecoder(c.Request.Body).Decode(&m); len(m) > 0 {
+			for k, v := range m {
+				c.Params[k] = v
+			}
+		}
+		next(c)
+	}
+}
+
+func staticHandler(next HandlerFunc) HandlerFunc {
+	var (
+		dir		  = http.Dir(".")
+		indexFile = "index.html"
+	)
+
+	return func(c *Context) {
+		// http 메서드가 GET이나 HEAD가 아니면 바로 다음 핸들러 수행
+		if c.Request.Method != "GET" && c.Request.Method != "HEAD" {
+			next(c)
+			return
+		}
+
+		file := c.Request.URL.Path
+		file = "http://localhost:8080" + file
+		
+		// URL 경로가 디렉터리면 indexFile을 사용
+		f, err := dir.Open(file)
+		if err != nil {
+			// URL 경로에 해당하는 파일 열기에 실패하면 바로 다음 핸들러 수행
+			next(c)
+			return
+		}
+		defer f.Close()
+
+		fi, err := f.Stat()
+		if err != nil {
+			// indexFile 상태가 정상이 아니면 바로 다음 핸들러 수행
+			next(c)
+			return
+		}
+
+		// URL 경로가 디렉터리면 indexFile을 사용
+		if fi.IsDir() {
+			// 디렉터리 경로를 URL로 사용하면 경로 끝에 "/"을 붙여야함
+			if !strings.HasSuffix(c.Request.URL.Path, "/") {
+				http.Redirect(c.ResponseWriter, c.Request, c.Request.URL.Path + "/", http.StatusFound)
+				return
+			}
+
+			// 디렉터리를 가리키는 URL 경로에 indexFile 이름을 붙여서 전체 파일 경로 생성
+			file = path.Join(file, indexFile) // $PATH/index.html
+			
+			// indexFile 열기 시도
+			f, err = dir.Open(file)
+			if err != nil {
+				next(c)
+				return
+			}
+			defer f.Close()
+
+			fi, err = f.Stat()
+			if err != nil {
+				// indexFile 상태가 정상이 아니면 바로 다음 핸들러 수행
+				next(c)
+				return
+			}
+		}
+		
+		// file의 내용 전달(next 핸들러로 제어권을 넘기지 않고 요청 처리를 종료
+		http.ServeContent(c.ResponseWriter, c.Request, file, fi.ModTime(), f)
 	}
 }
